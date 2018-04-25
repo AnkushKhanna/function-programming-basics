27a22c808dc715e30299a7dc8af679ce91054196
ankush.khanna <ankush.khanna@flixbus.com>
2018-04-25 09:24:09 +0200

    Another test.

--- src/test/scala/monads_example/MonadTest.scala
+++ src/test/scala/monads_example/MonadTest.scala
@@ -2,12 +2,15 @@ package monads_example
 
 import org.scalatest.FlatSpec
 
+import scala.util.Try
+
 class MonadTest extends FlatSpec {
 
   import Monad._
 
   "Filter" should "filter correctly for List[Int] monad" in {
-    val filteredList = op.filterM(List(1, 2, 3, 4, 5, 6))(i => Option(i % 2 == 0))
+    val filteredList = listMonad.filterM(List(1, 2, 3, 4, 5, 6))(n => List(n > 2))
+    println(filteredList)
     assert(filteredList.size === 3)
   }
 
