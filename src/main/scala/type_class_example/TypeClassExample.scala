8795cd1b57fee82d61c8d30a8e0b6f47e4a96547
ankush.khanna <ankush.khanna@flixbus.com>
2018-11-08 17:31:21 +0100

    Remove redundant comment.

--- src/main/scala/type_class_example/TypeClassExample.scala
+++ src/main/scala/type_class_example/TypeClassExample.scala
@@ -6,12 +6,7 @@ package type_class_example
 * One could say that the same could be achieved by extending a simple trait,
 * but with type classes it is not necessary to predict such a need beforehand.
 * */
-/*
-* Type class is a class (group) of types, which satisfy some contract defined in a trait with addition that such
-* functionality (trait and implementation) can be added without any changes to the original code.
-* One could say that the same could be achieved by extending a simple trait,
-* but with type classes it is not necessary to predict such a need beforehand.
-* */
+
 trait Show[A] {
   def show(a: A): String
 }
